bc9b3155d1a1bec5fc24c33ea0495703f458df27
Jianping Song <jianping.song@shopee.com>
2020-02-02 20:26:03 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: '单口喜剧表演手册',
+  description: 'Comedy Bring People Together',
+  base: '/stand-up-comedy/',
+  dest: "./dist",
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: '单立人喜剧', link: 'http://www.danlirencomedy.com/' },
+    ],
+    sidebar: [
+      '/',
+    ]
+  }
+}
